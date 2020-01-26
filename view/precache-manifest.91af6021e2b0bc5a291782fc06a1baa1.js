f12b31348894943635ce8566e97f7a2427744adc
zer0eXploit <37069426+zer0eXploit@users.noreply.github.com>
2020-01-26 16:28:36 +0630

    Add front end

---
+++ view/precache-manifest.91af6021e2b0bc5a291782fc06a1baa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f921bb511614cc09eaf95b1f0fdc196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe24d337a7a1d2b0783",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "819db871ec57b651bdfe",
+    "url": "/static/css/main.352305a6.chunk.css"
+  },
+  {
+    "revision": "2fe24d337a7a1d2b0783",
+    "url": "/static/js/2.5f4856ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5f4856ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "819db871ec57b651bdfe",
+    "url": "/static/js/main.e5f29fc0.chunk.js"
+  },
+  {
+    "revision": "b4aec04724051f326491",
+    "url": "/static/js/runtime-main.e35134c5.js"
+  }
+]);

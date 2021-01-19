5638a07abe556334d54eed410cb28c89831b4876
Yan Waipann <yanwaipann@gmail.com>
2021-01-19 22:18:43 +0630

    Remove API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require("clarifai");
 
 const clarifaiApp = new Clarifai.App({
-  apiKey: process.env.CLARIFAI_API_KEY;
+  apiKey: process.env.CLARIFAI_API_KEY
 });
 
 const handleApiCall = async (req, res) => {
